1de1bdabae47bcde89420bdf042201e7a23ecd0f
jfcui <jfcui@aibee.com>
2023-10-16 17:50:07 +0800

    add landing page and calendar page

---
+++ app/calender/page.tsx
@@ -0,0 +1,15 @@
+export default function Calender() {
+    return (
+        <p
+            style={{
+                fontSize: "1.2rem",
+                fontWeight: "bold",
+                color: "#000",
+                margin: "0",
+                padding: "0",
+            }}
+        >
+            Calender
+        </p>
+    );
+}
